28afb250ccb676db0650d0ab02598bed791641bf
jack <jack.rebussi@gmail.com>
2021-12-24 16:05:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,318 @@
+let position = null;
+
+// function line (letter){
+//     for (var num = 8; num = 1; num--){
+//         position = letter + num; 
+//         document.getElementById("x").innerText = position;
+//     }
+      
+// }
+
+
+
+
+function a8() {
+    position = `A8`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b8(){
+    position=`B8`
+    document.getElementById("x").innerText = position;
+}
+function c8(){
+    position=`C8`
+    document.getElementById("x").innerText = position;
+}
+
+function d8(){
+    position=`D8`
+    document.getElementById("x").innerText = position;
+}
+
+function e8(){
+    position=`E8`
+    document.getElementById("x").innerText = position;
+}
+
+function f8(){
+    position=`F8`
+    document.getElementById("x").innerText = position;
+}
+
+function g8(){
+    position=`G8`
+    document.getElementById("x").innerText = position;
+}
+
+function h8(){
+    position=`H8`
+    document.getElementById("x").innerText = position;
+}
+
+function a7() {
+    position = `A7`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b7(){
+    position=`B7`
+    document.getElementById("x").innerText = position;
+}
+function c7(){
+    position=`C7`
+    document.getElementById("x").innerText = position;
+}
+
+function d7(){
+    position=`D7`
+    document.getElementById("x").innerText = position;
+}
+
+function e7(){
+    position=`E7`
+    document.getElementById("x").innerText = position;
+}
+
+function f7(){
+    position=`F7`
+    document.getElementById("x").innerText = position;
+}
+
+function g7(){
+    position=`G7`
+    document.getElementById("x").innerText = position;
+}
+
+function h7(){
+    position=`H7`
+    document.getElementById("x").innerText = position;
+}
+function a6() {
+    position = `A6`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b6(){
+    position=`B6`
+    document.getElementById("x").innerText = position;
+}
+function c6(){
+    position=`C6`
+    document.getElementById("x").innerText = position;
+}
+
+function d6(){
+    position=`D6`
+    document.getElementById("x").innerText = position;
+}
+
+function e6(){
+    position=`E6`
+    document.getElementById("x").innerText = position;
+}
+
+function f6(){
+    position=`F6`
+    document.getElementById("x").innerText = position;
+}
+
+function g6(){
+    position=`G6`
+    document.getElementById("x").innerText = position;
+}
+
+function h6(){
+    position=`H6`
+    document.getElementById("x").innerText = position;
+}
+function a5() {
+    position = `A5`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b5(){
+    position=`B5`
+    document.getElementById("x").innerText = position;
+}
+function c5(){
+    position=`C5`
+    document.getElementById("x").innerText = position;
+}
+
+function d5(){
+    position=`D5`
+    document.getElementById("x").innerText = position;
+}
+
+function e5(){
+    position=`E5`
+    document.getElementById("x").innerText = position;
+}
+
+function f5(){
+    position=`F5`
+    document.getElementById("x").innerText = position;
+}
+
+function g5(){
+    position=`G5`
+    document.getElementById("x").innerText = position;
+}
+
+function h5(){
+    position=`H5`
+    document.getElementById("x").innerText = position;
+}
+function a4() {
+    position = `A4`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b4(){
+    position=`B4`
+    document.getElementById("x").innerText = position;
+}
+function c4(){
+    position=`C4`
+    document.getElementById("x").innerText = position;
+}
+
+function d4(){
+    position=`D4`
+    document.getElementById("x").innerText = position;
+}
+
+function e4(){
+    position=`E4`
+    document.getElementById("x").innerText = position;
+}
+
+function f4(){
+    position=`F4`
+    document.getElementById("x").innerText = position;
+}
+
+function g4(){
+    position=`G4`
+    document.getElementById("x").innerText = position;
+}
+
+function h4(){
+    position=`H4`
+    document.getElementById("x").innerText = position;
+}
+function a3() {
+    position = `A3`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b3(){
+    position=`B3`
+    document.getElementById("x").innerText = position;
+}
+function c3(){
+    position=`C3`
+    document.getElementById("x").innerText = position;
+}
+
+function d3(){
+    position=`D3`
+    document.getElementById("x").innerText = position;
+}
+
+function e3(){
+    position=`E3`
+    document.getElementById("x").innerText = position;
+}
+
+function f3(){
+    position=`F3`
+    document.getElementById("x").innerText = position;
+}
+
+function g3(){
+    position=`G3`
+    document.getElementById("x").innerText = position;
+}
+
+function h3(){
+    position=`H3`
+    document.getElementById("x").innerText = position;
+}
+function a2() {
+    position = `A2`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b2(){
+    position=`B2`
+    document.getElementById("x").innerText = position;
+}
+function c2(){
+    position=`C2`
+    document.getElementById("x").innerText = position;
+}
+
+function d2(){
+    position=`D2`
+    document.getElementById("x").innerText = position;
+}
+
+function e2(){
+    position=`E2`
+    document.getElementById("x").innerText = position;
+}
+
+function f2(){
+    position=`F2`
+    document.getElementById("x").innerText = position;
+}
+
+function g2(){
+    position=`G2`
+    document.getElementById("x").innerText = position;
+}
+
+function h2(){
+    position=`H2`
+    document.getElementById("x").innerText = position;
+}
+function a1() {
+    position = `A1`;
+    document.getElementById("x").innerText = position;    
+}
+
+function b1(){
+    position=`B1`
+    document.getElementById("x").innerText = position;
+}
+function c1(){
+    position=`C1`
+    document.getElementById("x").innerText = position;
+}
+
+function d1(){
+    position=`D1`
+    document.getElementById("x").innerText = position;
+}
+
+function e1(){
+    position=`E1`
+    document.getElementById("x").innerText = position;
+}
+
+function f1(){
+    position=`F1`
+    document.getElementById("x").innerText = position;
+}
+
+function g1(){
+    position=`G1`
+    document.getElementById("x").innerText = position;
+}
+
+function h1(){
+    position=`H1`
+    document.getElementById("x").innerText = position;
+}
